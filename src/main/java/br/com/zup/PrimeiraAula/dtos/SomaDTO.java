d16c01da339d62209b16bb2c71423dfd8514635e
Vinícius <vinicius.raymundo@zup.com.br>
2021-07-28 15:57:20 -0300

    Crair classe DTO para Soma

---
+++ src/main/java/br/com/zup/PrimeiraAula/dtos/SomaDTO.java
@@ -0,0 +1,25 @@
+package br.com.zup.PrimeiraAula.dtos;
+
+public class SomaDTO {
+    private int primeiroNumero;
+    private int segundoNumero;
+
+    public SomaDTO() {
+    }
+
+    public int getPrimeiroNumero() {
+        return primeiroNumero;
+    }
+
+    public void setPrimeiroNumero(int primeiroNumero) {
+        this.primeiroNumero = primeiroNumero;
+    }
+
+    public int getSegundoNumero() {
+        return segundoNumero;
+    }
+
+    public void setSegundoNumero(int segundoNumero) {
+        this.segundoNumero = segundoNumero;
+    }
+}
