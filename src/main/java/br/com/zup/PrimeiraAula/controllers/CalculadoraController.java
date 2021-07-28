0e93bf868c603cde1d448282d668b7c7d787a5a4
Vinícius <vinicius.raymundo@zup.com.br>
2021-07-28 15:57:36 -0300

    implementar metodo de soma

--- src/main/java/br/com/zup/PrimeiraAula/controllers/CalculadoraController.java
+++ src/main/java/br/com/zup/PrimeiraAula/controllers/CalculadoraController.java
@@ -1,7 +1,15 @@
 package br.com.zup.PrimeiraAula.controllers;
 
+import br.com.zup.PrimeiraAula.dtos.SomaDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculadoraController {
+
+    @PostMapping("/soma")
+    public int soma(@RequestBody SomaDTO somaDTO){
+        return somaDTO.getPrimeiroNumero() + somaDTO.getSegundoNumero();
+    }
 }
