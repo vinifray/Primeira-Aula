21c88e60d8e2a821cf5600e9c8d2d6e18ec1d9d0
Vinícius <vinicius.raymundo@zup.com.br>
2021-07-28 15:39:48 -0300

    Crair projeto de estudo

---
+++ src/main/java/br/com/zup/PrimeiraAula/PrimeiraAulaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.PrimeiraAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/PrimeiraAula/PrimeiraAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.PrimeiraAula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
